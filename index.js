d5405e464cf7bb94a3f0f1ac923b9be9f2e6043c
Gordon OmLiber <145753989+copcilufaranume@users.noreply.github.com>
2024-08-09 11:39:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const video = document.getElementById("bgVideo");
+video.volume = 0.5;
+
+function muteOrUnmute() {
+    if (event.key === "m" && video.muted == false) {
+        video.muted = true;
+    } else if (event.key === "m" && video.muted == true) {
+        video.volume = 0.5;
+        video.muted = false;
+    }
+}
